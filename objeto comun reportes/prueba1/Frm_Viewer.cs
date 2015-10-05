c8d0af0532a8c519dc86b6b505f2127e110cc313
José Enrique Arredondo Revolorio <arredondoe10@gmail.com>
2015-10-05 16:48:45 -0600

    proyecto reportes
    archivos de objeto comun de reportes para el proceso industrial de
    colchoneria

---
+++ objeto comun reportes/prueba1/Frm_Viewer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba1
+{
+    public partial class Frm_Viewer : Form
+    {
+        public Frm_Viewer()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Viewer_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
